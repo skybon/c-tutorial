f35f8ca7ff2dab023e72dde19ec975e6b97d1795
Artem Vorotnikov <artem@vorotnikov.me>
2014-12-05 16:53:21 +0300

    0.2

--- pbm-generator/pbm-generator.c
+++ pbm-generator/pbm-generator.c
@@ -9,19 +9,19 @@ int canvas[100][100];							//сам холст
 struct Timage a;
 struct Timage *pimg;
 
-void draw_horizontal_line(int hline_y,int hline_length) {		//горизонтальная линия длиной в холст
+void draw_horizontal_line(struct Timage *p,int hline_y,int hline_length) {		//горизонтальная линия длиной в холст
 	int x;								//счётчик
 
 	for (x=0;x<hline_length;x++) {
-		a.canvas[x][hline_y]=1;
+		p->canvas[x][hline_y]=1;
 	}
 }
 
-void draw_vertical_line(int vline_x,int vline_length) {			//вертикальная линия длиной в холст
+void draw_vertical_line(struct Timage *p,int vline_x,int vline_length) {			//вертикальная линия длиной в холст
 	int y;								//счётчик
 
 	for (y=0;y<vline_length;y++){
-		a.canvas[vline_x][y]=1;
+		p->canvas[vline_x][y]=1;
 	}
 }
 
@@ -37,34 +37,34 @@ void draw_grid(){
 
 	for (ypos=0;ypos<grid_width;ypos++){
 		if (ypos % (cell_width+1) == 0){
-			draw_vertical_line(ypos, grid_height);
+			draw_vertical_line(pimg,ypos, grid_height);
 		}
 	}
 
 	for (hpos=0;hpos<grid_height;hpos++){
 		if (hpos % (cell_height+1) == 0){			//+1 т.к. 1 пиксель между ячейками (граница)
-			draw_horizontal_line(hpos, grid_width);
+			draw_horizontal_line(pimg,hpos, grid_width);
 		}
 	}
 }
 
 void show(struct Timage* p) {
-    pimg=&a;
-	printf("P1\n##################################\n#\n# Создано следующее PBM-изображение:\n#\n##################################\n#\n# CREATED BY PBM GENERATOR 0.1\n# (c) Artem Vorotnikov <artem@vorotnikov.me>, 2014\n#\n##################################\n#\n%i %i\n", a.width, a.height);
+	printf("P1\n##################################\n#\n# Создано следующее PBM-изображение:\n#\n##################################\n#\n# CREATED BY PBM GENERATOR 0.1\n# (c) Artem Vorotnikov <artem@vorotnikov.me>, 2014\n#\n##################################\n#\n");
+	printf("%i %i \n", p->width, p->height);
 	int x;
 	int y;
 
-	for (y=0;y<a.height;y++){
-		for(x=0;x<a.width;x++){
-			printf("%d ",a.canvas[x][y]);
+	for (y=0;y<p->height;y++){
+		for(x=0;x<p->width;x++){
+			printf("%d ",p->canvas[x][y]);
 		}
 		printf("\n");
 	}
 }
 
 void init(struct Timage *p,int w, int h) {
-    a.width=w;
-    a.height=h;
+    p->width=w;
+    p->height=h;
 }
 
 int main(){
